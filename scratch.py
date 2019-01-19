ef665e9aaeaab1c5cbb69f5b809633ca12e44078
Tae_woong <ytw5754@naver.com>
2019-01-20 02:14:59 +0900

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -5,6 +5,10 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('C:\python/chromedriver')
 
+def wait():
+    driver.implicitly_wait(3)
+def long_wait():
+    driver.implicitly_wait(8)
 
 def ExcelExpress():
     wb= Workbook()
@@ -16,9 +20,9 @@ def ExcelExpress():
             ws1.cell(row=row, column=col, value=int("{}{}".format(row,col)))
 
     wb.save("C:\study/test.xlsx")
-
+##엑셀파일
 def Login():
-    driver.implicitly_wait(3)
+    wait()
     driver.get('https://www.hotdealzone.co.kr/')
 
     driver.find_element_by_name('mb_id').send_keys('ytw1122')
@@ -30,12 +34,17 @@ def ReadInfo():
     driver.find_element_by_xpath('//*[@id="main_wide"]/div[2]/div[1]/div[2]/span[1]/a').click()
     driver.find_element_by_xpath('//*[@id="content_wide"]/div/div[1]/ul/li[1]/a').click()
 
+    long_wait()
     html=driver.page_source
     soup=BeautifulSoup(html,'html.parser')
-    # item_name=soup.select('#nt_order_list_member > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr > td.tracking > div:nth-child(1) > b')
 
-    # for item in item_name:
-    #     print(item.text)
+    my_titles=driver.find_elements_by_class_name("auto_jak")
+    #
+    # my_titles=soup.select(
+    #     '#nt_order_list_member > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr > td.tracking > div:nth-child(1)'
+    # )
+    for item in my_titles:
+        print(item.text)
 ExcelExpress()
 Login()
 ReadInfo()
