c5da156d547282f7d01a20d3642ebf319b751302
Tae_woong <ytw5754@naver.com>
2019-03-10 01:54:49 +0900

    btn connect
    btn connect

--- StoreFarmCrawling.py
+++ StoreFarmCrawling.py
@@ -4,14 +4,16 @@ import gui
 from selenium import webdriver
 from datetime import date
 import os
-import time
 from multiprocessing import Pool
 
-options = None
 # 헤드리스 모드 진행시 블록아웃을 삭제
-driver = None
 checkList_info = ("아이디", "이름", "집주소", "전화번호", "통관번호")
 customer_info_arr = []  # Customer 클래스의 배열
+
+options=webdriver.ChromeOptions()
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+driver=webdriver.Chrome('C:\python/chromedriver',options=options)
 
 class Customer:
 
@@ -62,8 +64,8 @@ def open_info_pages(url):
         driver.implicitly_wait(3)
         driver.get(url)
         read_spec_order()
-    except:
-        print("상세정보창을 열 수 없습니다")
+    except Exception as ex:
+        print("상세정보창을 열 수 없습니다",ex)
         exit()
 
 
@@ -89,6 +91,9 @@ def read_order_and_return_customer_count():
     for item in list:
         content.append(item.text)
     customers_count=int(len(list))
+    if customers_count==0:
+        print("오늘은 하나도 팔리지않았군요")
+        exit()
 
     #TODO : list 배열에서 title 값을 읽어오는 역할을 수행한다.
     for customer_num in range(0,customers_count):
@@ -101,42 +106,42 @@ def read_order_and_return_customer_count():
     return customers_count
 
 def log_in(email,password):
-    try:
-        driver.implicitly_wait(3)
-        driver.get('https://sell.smartstore.naver.com/#/login')
-        driver.find_element_by_id('loginId').send_keys(email)
-        driver.find_element_by_id('loginPassword').send_keys(password)
-        driver.find_element_by_xpath('//*[@id="loginButton"]').click()
-    #     TODO: 여기에 사용자의 이메일과 아이디를 넣어줘야 작동하도록 구현
-        return True
-    except Exception as inst:
+    driver.implicitly_wait(3)
+    driver.get('https://sell.smartstore.naver.com/#/login')
+    driver.find_element_by_id('loginId').send_keys(email)
+    driver.find_element_by_id('loginPassword').send_keys(password)
+    driver.find_element_by_xpath('//*[@id="loginButton"]').click()
+    driver.implicitly_wait(3)
 
-        print("fuck, its not working")
-        return False
 
 #     in case when login doesnt work properly
+def log_in_check():
+    if driver.current_url=='https://sell.smartstore.naver.com/#/login':
+        print("로그인 실패 수고링")
+        return False
+    return True
 
 def go_to_order_page():
-    drop_down_menu = driver.find_element_by_xpath('//*[@id="seller-lnb"]/div/div[1]/ul/li[3]')
-    drop_down_menu.click()
-    driver.implicitly_wait(3)
-    driver.find_element_by_css_selector(
+    if log_in_check():
+        exit()
+    try:
+        driver.implicitly_wait(3)
+        drop_down_menu = driver.find_element_by_xpath('//*[@id="seller-lnb"]/div/div[1]/ul/li[3]')
+        drop_down_menu.click()
+        driver.implicitly_wait(3)
+        driver.find_element_by_css_selector(
         '#seller-lnb > div > div:nth-child(1) > ul > li.active > ul > li:nth-child(1) > a').click()
+    except Exception as ex:
+        print("실패",ex)
+        exit()
 
-# webdriver.ChromeOptions()
-# options.add_argument('window-size=1920x1080')
-# options.add_argument("disable-gpu")
-# driver=webdriver.Chrome('C:\python/chromedriver',options=options)
 def start_core_service(email,password):
-
-    start_time = time.time()
     log_in(email,password)
     driver.implicitly_wait(3)
     go_to_order_page()
     customer_count = read_order_and_return_customer_count()
     file_name = os.path.dirname(os.path.realpath(__file__)) + get_excel_file_name()
     make_excel(file_name, customer_count)
-    print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__=='__main__':
     # # TODO : 속도가 너무 느리다. 멀티프로세싱으로 좀 더 빠른 방법이 없을까?
--- gui.py
+++ gui.py
@@ -11,21 +11,34 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        email = QLineEdit(self)
-        email.move(20, 50)
+        # 이메일은 보이도록 설정
+        self.email_input = QLineEdit(self)
+        self.email_input.move(20, 50)
 
-        password = QLineEdit(self)
-        password.move(20, 100)
+        # 비번은 안보이도록 설정
+        self.password_input = QLineEdit(self)
+        self.password_input.move(20, 100)
 
-        log_in_btn=QPushButton('로그인',self)
-        try:
-            log_in_btn.clicked.connect(self.log_in)
-        except:
-            print("로그인에 실패하였습니다")
+        self.log_in_btn = QPushButton('로그인', self)
 
-        self.setWindowTitle('QLineEdit')
+        self.log_in_btn.setToolTip("로그인을 하려면 클릭하세요")
+
+        self.log_in_btn.move(200, 100)
+        self.setWindowTitle('크롤링')
         self.setGeometry(300, 300, 300, 200)
         self.show()
 
-    def log_in(self,email,password):
-
+        try:
+            self.log_in_btn.clicked.connect(self.log_in)
+        except Exception as ex:
+            print("로그인에 실패하였습니다", ex)
+            exit()
+    #      TODO: 로그인 실패여부를 확인하는 함수를 만들어야한다
+    def log_in(self):
+        email = self.email_input.text()
+        password = self.password_input.text()
+        try:
+            StoreFarmCrawling.start_core_service(email, password)
+        except Exception as ex:
+            print("말이 안됌", ex)
+            exit()
